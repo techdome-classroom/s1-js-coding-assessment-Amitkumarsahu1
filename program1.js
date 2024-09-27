7a9bcd08135675e17ebc21d00513cfd9bf362b5c
Amitkumarsahu1 <amitsahu91796@gmail.com>
2024-09-27 12:17:41 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,11 @@
 const getTotalIsles = function (grid) {
 
 
-  
+  if (grid === null || grid.length === 0) return 0;
+
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let islandCount = 0;
 
 };
 
