3455be1f38a4056d3109d072886894bca27010d4
Amitkumarsahu1 <amitsahu91796@gmail.com>
2024-09-27 12:19:04 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,18 +1,6 @@
 const getTotalIsles = function (grid) {
 
 
-  if (grid === null || grid.length === 0) return 0;
-
-    let rows = grid.length;
-    let cols = grid[0].length;
-    let islandCount = 0;
-
-    function dfs(r, c) {
-      
-      if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === 'W') {
-          return;
-      }
-      grid[r][c] = 'W';
 };
 
 module.exports = getTotalIsles;
